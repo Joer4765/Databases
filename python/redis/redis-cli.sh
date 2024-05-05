6f4e160ad9524c87673d58320b535b91282edc7d
Oleksandr Holan <game.play.for.alex@gmail.com>
2024-05-05 20:02:34 +0300

    redis cli script done

--- python/redis/redis-cli.sh
+++ python/redis/redis-cli.sh
@@ -1,51 +1,46 @@
 #!/bin/bash
 
-# Connecting to redis server. Update the host and port as per your Redis server configuration
-redis-cli -h localhost -p 6379
+# Task 1.1
+redis-cli set students:oleksandr "{name: 'My Name', university: 'CHNU', group: 'IPZ243', id: 1}"
+redis-cli get students:oleksandr
 
-# Task 1.1: Create a key with your details and get the value
-redis-cli SET students:myname "{name: 'My Name', university: 'CHNU', group: 'IPZ243', id: 1}"
-redis-cli GET students:myname
+# Task 1.2
+redis-cli strlen students:oleksandr
+redis-cli getrange students:oleksandr 1 40
+redis-cli setrange students:oleksandr 41 " extra !data!"
+redis-cli get students:oleksandr
 
-# Task 1.2: Get the length of the value, extract a substring and update the value
-redis-cli STRLEN students:myname
-redis-cli GETRANGE students:myname 1 40
-redis-cli APPEND students:myname " extra !data!"
-redis-cli GET students:myname
+# Task 1.3
+redis-cli hset students:hash_oleksandr name "Oleksandr" university "CHNU" group "IPZ243" id 1 # hash set
+redis-cli hset students:hash_oleksandr age 20
+redis-cli hgetall students:hash_oleksandr
+redis-cli hget students:hash_oleksandr id
+redis-cli hdel students:hash_oleksandr university
+redis-cli hgetall students:hash_oleksandr
 
-# Task 1.3: Create a hash table, add a field, get all fields and values, get field 'id', and delete field 'university'
-redis-cli HMSET students:hash_myname name "My Name" university "CHNU" group "IPZ243" id 1
-redis-cli HSET students:hash_myname age 20
-redis-cli HGETALL students:hash_myname
-redis-cli HGET students:hash_myname id
-redis-cli HDEL students:hash_myname university
+# Task 1.4
+redis-cli lpush actions:oleksandr filter:1 delete:2 update:3 read:4 create:5 # list
+redis-cli lrange actions:oleksandr 0 -1
+redis-cli ltrim actions:oleksandr -3 -1
+redis-cli lrange actions:oleksandr 0 -1
 
-# Task 1.4: Create a list to describe a sequence of actions, get the list, and trim the list
-redis-cli LPUSH actions:myname "open:page1" "open:page2" "order:ticket" "order:ticket2" "buy:all"
-redis-cli LRANGE actions:myname 0 -1
-redis-cli LTRIM actions:myname -3 -1
-redis-cli LRANGE actions:myname 0 -1
+# Task 1.5
+redis-cli sadd subjects:1 "Math" "Physics" "Chemistry" "Biology" "English" # set
+redis-cli sadd subjects:2 "History" "Geography" "Math" "Literature" "Physics"
+redis-cli sismember subjects:1 "Math"
+redis-cli sismember subjects:2 "Math"
+redis-cli sinterstore common_subjectss subjects:1 subjects:2
+redis-cli smembers common_subjectss
 
-# Task 1.5: Create sets for subjects in two semesters, check membership, identify common subjects
-redis-cli SADD subject:1 "Math" "Physics" "Chemistry" "Biology" "English"
-redis-cli SADD subject:2 "History" "Geography" "Math" "Literature" "Physics"
-redis-cli SISMEMBER subject:1 "Math"
-redis-cli SISMEMBER subject:2 "Math"
-redis-cli SINTERSTORE common_subjects subject:1 subject:2
-redis-cli SMEMBERS common_subjects
+# Task 1.6
+redis-cli zadd subjects 75 "Math" 80 "Physics" 65 "Chemistry" 85 "Biology" 70 "English" # sorted set
+redis-cli zcount subjects 71 inf
+redis-cli zrevrank subjects "Physics" # Reversed ranking from 0
 
-# Task 1.6: Create a sorted set of subjects with scores, find count of subjects with score > 70, get reverse rank
-redis-cli ZADD subjects 75 "Math" 80 "Physics" 65 "Chemistry" 85 "Biology" 70 "English"
-redis-cli ZCOUNT subjects 71 inf
-redis-cli ZREVRANK subjects "Physics"
-
-# Task 1.7: Create expiring keys
-redis-cli SET temporary:key "This Key Will Expire"
-redis-cli EXPIRE temporary:key 60 # expires after 60 seconds
-redis-cli SET expiry:key "This Key Will Expire on Birthday"
-redis-cli EXPIREAT expiry:key 1746076800 # Assuming the birthday timestamp as example
-redis-cli TTL temporary:key
-redis-cli TTL expiry:key
-
-# Exit
-exit
+# Task 1.7
+redis-cli set temporary:key "This Key Will Expire"
+redis-cli expire temporary:key 60
+redis-cli set birth:key "This Key Will Expire on Birthday"
+redis-cli expireat birth:key 1746076800 # timestamp
+redis-cli ttl temporary:key
+redis-cli ttl berth:key # time to live
